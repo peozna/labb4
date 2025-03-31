c4f9a2bcae2887cf84b86b0f93c96ffb2f5f265e
Nathalie <nannaskrutt@gmail.com>
2025-03-31 15:07:38 +0200

    tagit bort dubbla element i footer

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
 
-<!DOCTYPE html>
-<html lang="sv">
-<head>
-    <title>Footer</title>
     <link rel="stylesheet" href="mystyle.css">
-</head>
+
 <!-- Footer som används på alla sidor -->
 <footer>
     <p>
