48565d469ee2c860126ee683bad817effd924667
peozna <147834990+peozna@users.noreply.github.com>
2025-05-19 14:36:17 +0200

    Update db_credentials.php

--- db_credentials.php
+++ db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_SERVER", "localhost");
 define("DB_USER", "nattrn2");
-define("DB_PASS", "Sjsdefcjxnc7xyl");
+define("DB_PASS", "Sjsdefcjxznc7xyl");
 define("DB_NAME", "d0019e_vt25_nattrn2");
 ?>
